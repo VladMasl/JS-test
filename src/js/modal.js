c82e6f50dbfc231c79262d592f7039471113870e
Vlad  Masliuk <102538039+VladMasl@users.noreply.github.com>
2022-09-20 13:18:45 +0300

    close modal click esc

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,17 +3,24 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    backdropEl: document.querySelector('.backdrop'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.backdropEl.addEventListener('click', onCloseModalClickBackdrop);
 
-  const backdropEl = document.querySelector('.backdrop');
-  backdropEl.addEventListener('click', onCloseModalClickBackdrop);
   function onCloseModalClickBackdrop(e) {
-    if (e.target !== backdropEl) return;
+    if (e.target !== refs.backdropEl) return;
     refs.modal.classList.add('is-hidden');
   }
+
+  window.addEventListener('keydown', evt => {
+    const isEscKey = evt.code === 'Escape';
+    if (isEscKey) {
+      refs.modal.classList.add('is-hidden');
+    }
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
